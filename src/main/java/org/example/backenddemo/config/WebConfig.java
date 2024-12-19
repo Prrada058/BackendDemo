c9ba2ad9852fa6445f8ece0718508496cbba0253
zhangxiaofan <prrada058@gmail.com>
2024-12-19 16:38:38 +0800

    add CORS策略

--- src/main/java/org/example/backenddemo/config/WebConfig.java
+++ src/main/java/org/example/backenddemo/config/WebConfig.java
@@ -2,6 +2,7 @@ package org.example.backenddemo.config;
 
 import org.example.backenddemo.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor( new LoginInterceptor()).addPathPatterns("/user/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:63343")  // 允许指定域名
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // 允许的请求方法
+                .allowedHeaders("*");
     }
 }
