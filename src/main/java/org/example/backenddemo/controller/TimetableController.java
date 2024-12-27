dfc09b08120f798400457abbc996d45005e376e6
zhangxiaofan <prrada058@gmail.com>
2024-12-27 14:56:43 +0800

    适配打包

--- src/main/java/org/example/backenddemo/controller/TimetableController.java
+++ src/main/java/org/example/backenddemo/controller/TimetableController.java
@@ -33,7 +33,7 @@ public class TimetableController {
 //        return now;
 //    }
 
-    @GetMapping("/timet")
+    @GetMapping("/timeBack")
     public List<String> getTimetables() {
         List<String> timetables = timetableMapper.selectTimetable();
         LocalTime now = LocalTime.now();
